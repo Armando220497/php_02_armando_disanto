187de8ef7f565d9e91334fd28875ed91a0e823c1
Armando Di Santo <armando.di.santo@hotmail.com>
2024-08-09 13:46:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -59,3 +59,22 @@ function calcolaMediaNumeriPari($array)
 
 $media = calcolaMediaNumeriPari($numbers);
 echo "La media dei numeri pari è: " . $media;
+
+
+
+echo "\n\n";
+// Selfwork PHP 6
+
+
+for ($i = 1; $i <= 100; $i++) {
+
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "HACKADEMY\n";
+    } elseif ($i % 3 == 0) {
+        echo "PHP\n";
+    } elseif ($i % 5 == 0) {
+        echo "JAVASCRIPT\n";
+    } else {
+        echo $i . "\n";
+    }
+}
